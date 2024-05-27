69221ef58c103f08076ac3ce534fbf2cc083fb7d
lkris2uic <lkris2@icloud.com>
2024-05-28 03:53:15 +0530

    Features

---
+++ src/components/features.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Tilt from 'react-tilt'
+import {motion} from 'framer-motion'
+
+import {styles} from '../styles';
+import {services} from '../constants'
+import {fadeIn, textVariant} from '../constants/motion'
+
+const features = () => {
+    return (<div>Meow</div>)
+}
+
+export default features
---
+++ src/constants/index.js
@@ -0,0 +1,21 @@
+
+  
+  
+const services = [
+    {
+      title: "Web Developer"
+    },
+    {
+      title: "React Native Developer"
+    },
+    {
+      title: "Backend Developer"
+    },
+    {
+      title: "Content Creator"
+    },
+  ];
+  
+  
+  
+  export { services };
