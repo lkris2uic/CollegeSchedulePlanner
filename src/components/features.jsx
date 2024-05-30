d1f238d659a0be12cf822d4f83b30a5d5edbe5a4
lkris2 <lkris2@icloud.com>
2024-05-30 15:54:23 +0530

    features css solved

--- src/components/features.jsx
+++ src/components/features.jsx
@@ -1,22 +1,25 @@
-import React from 'react'
-// import Tilt from 'react-tilt'
-// import {motion} from 'framer-motion'
-
-import {styles} from '../styles';
-// import {services} from '../constants'
-// import '/Users/lakshmikrishnan/planner-pro/src/cssFiles/features.scss'
-import {fadeIn, textVariant} from '../constants/motion'
+import { Container, Row, Col } from 'react-bootstrap';
 
 const Features = () => {
   return (
     <>
-      <h1 >Features</h1>
-      <p classname = 'features-heading'>
-        Say goodbye to the hassle of manually planning your semester! Our Automated Schedule Generation Tool is designed to streamline and 
-        personalize your course scheduling process. By leveraging advanced algorithms and optimization techniques, we create the best possible course schedules tailored to your unique 
-        preferences and academic goals.
-      </p>
+      <section className="banner" id="home">
+        <Container>
+          <Row className="align-items-center">
+            <Col xs={12} md={6} xl={7}>
+              <h1 className="tagline">Features</h1>
+              <p>
+                Say goodbye to the hassle of manually planning your semester! Our Automated Schedule Generation Tool is designed to streamline and
+                personalize your course scheduling process. By leveraging advanced algorithms and optimization techniques, we create the best possible course schedules tailored to your unique
+                preferences and academic goals.
+              </p>
+            </Col>
+          </Row>
+          <div className = "mt-20 flex flex-wrap  gap-1-">heyyy</div>
+        </Container>
+      </section>
     </>
   );
 };
-export default Features
+
+export default Features;
